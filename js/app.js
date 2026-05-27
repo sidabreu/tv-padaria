b916105b676680b033944bcd98fbfa05210ec9a3
sidabreu <sid_abreu@hotmail.com>
2026-05-27 12:54:10 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ async function carregarConteudo() {
 
     try {
 
-        const resposta = await fetch("/tv-padaria/dados/conteudo.json");
+        const resposta = await fetch("/tv-padaria/data/conteudo.json");
 
         const dados = await resposta.json();
 
